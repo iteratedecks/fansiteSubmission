2d27a0b88a1e99aa64c7d1db7e771e400e7ff0a0
9andor@gmail.com <9andor@gmail.com>
2013-03-11 11:13:37 +0800

    Support Tyrant Optimizer.

--- fansiteConfig.py
+++ fansiteConfig.py
@@ -1,16 +1,20 @@
-import argparse
+import sys
 import os
+import argparse
 
 def getDefaultConfig():
-    config = []
-    config.append("# Config file for use with Fansite submission script\n")
-    config.append("# your simulation token (provided by the Fansite)")
-    config.append("token=00000000000000000000000000000000")
-    config.append("# the name of the simulator")
-    config.append("simulator=iteratedecks")
-    config.append("# the number of simulations per deck")
-    config.append("numSims=200000")
-    return("\n".join(config))
+    return """\
+# Config file for use with Fansite submission script
+
+# your simulation token (provided by the Fansite)
+token=00000000000000000000000000000000
+# the name of the simulator
+simulator=Tyrant Optimizer
+# the number of simulations per deck
+numSims=1000000
+# the number of CPU threads for simulation
+numThreads=1
+"""
 
 def getArgs():
     args = getCommandArgs()
@@ -19,45 +23,33 @@ def getArgs():
 
 def getCommandArgs():
     argParser = argparse.ArgumentParser(description='Submit simulator results to the Fansite.')
-    argParser.add_argument('-t', '--test', type=int, nargs='?', default=0, const=1, help='run against test repo')
+    argParser.add_argument('-t', '--test', action='store_const', default=0, const=1, help='run against test repo')
     argParser.add_argument('--config', default='fansite_config.txt', help='path to config file')
-    argParser.add_argument('--token', default='', help='simulator token')
-    argParser.add_argument('--simulator', default='', help='simulator used')
-    argParser.add_argument('--numSims', default='', help='number of simulations to run per deck')
-
+    argParser.add_argument('--token', help='simulator token')
+    argParser.add_argument('--simulator', help='simulator used')
+    argParser.add_argument('--numSims', help='number of simulations to run per deck')
+    argParser.add_argument('--numThreads', help='number of CPU threads for simulation')
+    argParser.add_argument('--limit', default=None, help='amount of decks to simulate')
+    argParser.add_argument('--runForever', action='store_const', default=0, const=1, help='run forever (Ctrl-Break to stop)')
     args = argParser.parse_args()
-
     return args
 
-def getConfigArgs(args = {}, config = "fansite_config.txt"):
+def getConfigArgs(args, config = "fansite_config.txt"):
     contents = None
     if(not os.path.exists(config)):
         print("Could not find config file. Creating one with default settings.")
-        f = None
-        try:
-            f = open(config, 'w')
+        with open(config, 'w') as f:
             contents = getDefaultConfig()
             f.write(contents)
-        finally:
-            f.close()
     else:
-        f = None
-        try:
-            f = open(config, 'r')
+        with open(config, 'r') as f:
             contents = f.read()
-        finally:
-            f.close()
 
     for line in contents.splitlines():
         if(len(line) == 0 or line[0] == "#"):
             continue
         arg = line.split('=')
-
-        if(arg[0] == "token" and args.token == ''):
-            args.token = arg[1]
-        elif(arg[0] == "simulator" and args.simulator == ''):
-            args.simulator = arg[1]
-        elif(arg[0] == "numSims" and args.numSims == ''):
-            args.numSims = arg[1]
-
+        if getattr(args, arg[0]) is None:
+            setattr(args, arg[0], arg[1])
     return args
+
---
+++ simulatorTyrantOptimizer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import re
+import subprocess
+
+from fansiteSimulator import FansiteSimulator
+
+class SimulatorTyrantOptimizer(FansiteSimulator):
+    name = "Tyrant Optimizer"
+    executable = "tyrant_optimize.exe"
+    results_regex = re.compile(r"win%: [.\d]+ \((\d+) out of (\d+)\)\D+ANP: ([\d.]+)")
+    results_keys = ["wins", "total", "anp"]
+
+    def loadVersion(self):
+        commandArgs = [self.executable, "-version"]
+        result = subprocess.check_output(commandArgs)
+        return result.split()[2]
+
+    def processResults(self, results):
+        match = self.results_regex.search(results)
+        if match:
+            return dict(zip(self.results_keys, match.groups()))
+        print("ERROR: Cannot find results from output {{{\n", results, "}}}")
+
+    def addAchievement(self, commandArgs, achievementId, missionId):
+        commandArgs.append("Mission #%s" % missionId)
+        commandArgs.extend(["-A", str(achievementId)])
+
+    def addAttackingDeck(self, commandArgs, attackingDeck, attackingDeckCards):
+        commandArgs.append(attackingDeck)
+
+    def addBattlegroundId(self, commandArgs, battlegroundId):
+        commandArgs.extend(["-e", str(battlegroundId)])
+
+    def addCustom(self, commandArgs, custom):
+        commandArgs.append(custom)
+
+    def addExtraArgs(self, commandArgs, args):
+        commandArgs.extend(["-t", str(getattr(args, "numThreads", 1))])
+        commandArgs.extend(["sim", args.numSims])
+
+    def addMission(self, commandArgs, missionId):
+        commandArgs.append("Mission #%s" % missionId)
+
+    def addOrdered(self, commandArgs):
+        commandArgs.append("-r")
+
+    def addQuest(self, commandArgs, questId):
+        commandArgs.append("Quest #%s" % questId)
+
+    def addRaid(self, commandArgs, raidId):
+        commandArgs.append("Raid #%s" % raidId)
+
+    def addSurge(self, commandArgs):
+        commandArgs.append("-s")
+
